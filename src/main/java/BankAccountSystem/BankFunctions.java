fef694e0b6c227b6de4fb2baacc473fe36733a17
John Isaac Rizon <jirizon693@gmail.com>
2023-06-05 17:46:26 +0800

    More commits.
    Added user profile and working on bank functions

---
+++ src/main/java/BankAccountSystem/BankFunctions.java
@@ -0,0 +1,10 @@
+
+package BankAccountSystem;
+
+import  java.util.*;
+
+public class BankFunctions extends User {
+    
+    //public List<User> users = new List(); work in progress
+    
+}
---
+++ src/main/java/BankAccountSystem/User.java
@@ -0,0 +1,17 @@
+
+package BankAccountSystem;
+
+import java.util.*;
+
+
+public class User {
+    
+    public String Name;
+    public String AccNum;
+    public String email;
+    public String password;
+    public int mobilenum;
+    public float Balance;
+    
+    
+}
