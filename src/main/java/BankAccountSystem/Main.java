384d93d2c2ffd15253b7864dc4844daec3a66da4
Charles Aaron B. Cayabyab <116819063+CharlesAaron@users.noreply.github.com>
2023-07-14 02:37:53 +0800

    Reworkedregform

--- src/main/java/BankAccountSystem/Main.java
+++ src/main/java/BankAccountSystem/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
        //new ATMWelcomePage();
        new MainMenu();
+       //new Reworkedregform();
       
 //       new BankAccountLogin();
 //Rizon Tests
